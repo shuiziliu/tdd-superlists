24b14d2c029593d07592617144a158279a61980a
shuiziliu <shuiziliu@gmail.com>
2015-02-08 18:14:29 +0800

    set STATIC_ROOT in settings and haven't disable admin

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,13 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import unittest
 
 # class NewVisitorTest(unittest.TestCase):
-class NewVisitorTest(LiveServerTestCase):
+# class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	"""docstring for NewVisitorTest"""
 
 	def setUp(self):
