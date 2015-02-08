659e311d3213eca232211fbc462dd29203d4eedb
shuiziliu <shuiziliu@gmail.com>
2015-02-08 12:06:07 +0800

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_test):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_test, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
@@ -36,13 +41,14 @@ class NewVisitorTest(unittest.TestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# import time
 		# time.sleep(10)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		# table = self.browser.find_element_by_id('id_list_table')
+		# rows = table.find_elements_by_tag_name('tr')
+		# self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 		# self.assertTrue(
 		# 	any(row.text == '1: Buy peacock feathers' for row in rows),
 		# 	"New to-do item did not appear in table -- its text was:\n%s" % (table.text,)
@@ -55,10 +61,12 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to a fly')
+		# table = self.browser.find_element_by_id('id_list_table')
+		# rows = table.find_elements_by_tag_name('tr')
+		# self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		# self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
+from .models import Item
+
 def home_page(request):
-	return render(request, 'lists/home.html', {
-		'new_item_text': request.POST.get('item_text', ''),
-		})
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/')
+	# 	new_item_text = request.POST['item_text']
+	# 	Item.objects.create(text=new_item_text)
+	# else:
+	# 	new_item_text = ''
+	# item = Item()
+	# item.text = request.POST.get('item_text', '')
+	# item.save()
+	items = Item.objects.all()
+	return render(request, 'lists/home.html', {'items': items})
+	# return render(request, 'lists/home.html', {
+	# 	'new_item_text': new_item_text
+	# 	})
